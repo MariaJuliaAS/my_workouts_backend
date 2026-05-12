29048acf77daf4eca5b76f85791bae963efe350e
MariaJuliaAS <mariajuliadev@gmail.com>
2026-05-12 15:43:58 -0300

    feat(rate limit): Adiciona middleware rate limit para controlar quantidade de tentativas de login

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { CreateUserController } from "./controller/user/CreateUserController";
 import { AuthUserController } from "./controller/user/AuthUserController";
+import { rateLimiter } from "./middleware/rateLimiter";
 
 const router = Router();
 
 router.post("/user", new CreateUserController().handle)
-router.post("/user/auth", new AuthUserController().handle)
+router.post("/user/auth", rateLimiter, new AuthUserController().handle)
 
 export { router };
